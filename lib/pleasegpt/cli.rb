4277c7578f21608552d9958badef049c204daa7d
Rafael Moreira <rafamsilva@gmail.com>
2023-03-28 10:39:12 +1100

    refac+dev - added hightline/openai gem to request api_key on instalation - error handling for api request - private method for api request

--- lib/pleasegpt/cli.rb
+++ lib/pleasegpt/cli.rb
@@ -1,4 +1,20 @@
+# rubocop:disable Metrics/MethodLength
+require 'openai'
+require 'highline/import'
+
 module PleaseGPT
+  # call error when API request returns empty response or nil text
+  class Error < StandardError
+    def self.check_response(response)
+      if response.choices.empty?
+        raise PleaseGPT::Error, "OpenAI API request returned empty response"
+      elsif response.choices[0].text.nil?
+        raise PleaseGPT::Error, "OpenAI API request returned nil text"
+      end
+    end
+  end
+
+  # CLI class for PleaseGPT gem
   class CLI
     def run
       print 'pleasegpt> '
@@ -10,8 +26,7 @@ module PleaseGPT
       puts pleasegpt(input)
     end
 
-    private
-
+    # call OpenAI API and return response
     def pleasegpt(input)
       response = OpenAI::Completion.create(
         engine: 'davinci',
@@ -22,7 +37,15 @@ module PleaseGPT
         stop: "\n",
         api_key: ENV['OPENAI_API_KEY']
       )
+      Error.check_response(response)
       response.choices[0].text.strip
+    end
+
+    private
+
+    def api_key
+      @api_key ||= ENV['OPENAI_API_KEY'] || ask('Please enter your OpenAI API key:  ') { |q| q.echo = "*" }
     end
   end
 end
+# rubocop:enable Metrics/MethodLength
