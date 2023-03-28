f1dd620cd4a8d91563fd58c4eeefeb7742d0dbe9
Rafael Moreira <rafamsilva@gmail.com>
2023-03-28 13:42:49 +1100

    added requires and refac api_key self method

--- lib/pleasegpt.rb
+++ lib/pleasegpt.rb
@@ -1,5 +1,7 @@
 # rubocop:disable Metrics/MethodLength
 require 'openai'
+require 'highline/import'
+require 'colorize'
 
 # Module for PleaseGPT gem
 module PleaseGPT
@@ -7,26 +9,26 @@ module PleaseGPT
 
   # Module for OpenAI API requests
   module Api
-    def self.generate_text(input)
-      client = OpenAI::Client.new(access_token: api_key)
-      response = client.completions(
-        parameters: {
-        model: 'text-davinci-003',
-        prompt: "#{input}\n",
-        max_tokens: 300,
-        temperature: 0.5,
-        n: 1,
-        stop: "\n"
-        }
-      )
-      Error.check_response(response)
-      response.choices[0].text.strip.colorize(:blue)
-    end
-
     private_class_method def self.api_key
       @api_key ||= ENV['OPENAI_API_KEY'] || begin
         ask('Please enter your OpenAI API key:  ') { |q| q.echo = '*' }
       end
+    end
+
+    def self.generate_text(input)
+      client = OpenAI::Client.new(access_token: api_key)
+      response = client.completions(
+        parameters: {
+          model: 'text-davinci-003',
+          prompt: "#{input}\n",
+          max_tokens: 300,
+          temperature: 0.5,
+          n: 1,
+          stop: "\n"
+        }
+      )
+      Error.check_response(response)
+      response.choices[0].text.strip.colorize(:blue)
     end
   end
 
