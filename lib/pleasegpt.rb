74dd00dcca457dc12ebde1fc6baf6743b385ab58
Rafael Moreira <rafamsilva@gmail.com>
2023-03-28 11:39:45 +1100

    refac: moved classes to pleasegpt.rb

--- lib/pleasegpt.rb
+++ lib/pleasegpt.rb
@@ -1,5 +1,42 @@
+# rubocop:disable Metrics/MethodLength
 require 'openai'
 
 # Module for PleaseGPT gem
 module PleaseGPT
+  autoload :CLI, 'pleasegpt/cli'
+
+  # Module for OpenAI API requests
+  module Api
+    def self.generate_text(input)
+      response = OpenAI::Completion.create(
+        engine: 'davinci',
+        prompt: "#{input}\n",
+        max_tokens: 2048,
+        temperature: 0.5,
+        n: 1,
+        stop: "\n",
+        api_key: api_key
+      )
+      Error.check_response(response)
+      response.choices[0].text.strip.colorize(:blue)
+    end
+
+    private_class_method def self.api_key
+      @api_key ||= ENV['OPENAI_API_KEY'] || begin
+        ask('Please enter your OpenAI API key:  ') { |q| q.echo = '*' }
+      end
+    end
+  end
+
+  # Handling errors from OpenAI API requests and responses
+  module Error
+    def self.check_response(response)
+      if response.choices.empty?
+        raise PleaseGPT::Error, 'OpenAI API request returned empty response'
+      elsif response.choices[0].text.nil?
+        raise PleaseGPT::Error, 'OpenAI API request returned nil text'
+      end
+    end
+  end
 end
+# rubocop:enable Metrics/MethodLength
--- lib/pleasegpt/cli.rb
+++ lib/pleasegpt/cli.rb
@@ -1,7 +1,7 @@
-# rubocop:disable Metrics/MethodLength
 require 'openai'
 require 'highline/import'
 require 'colorize'
+require 'pleasegpt'
 
 module PleaseGPT
   # Gem command-line tool interface
@@ -15,46 +15,5 @@ module PleaseGPT
       end
       puts Api.generate_text(input)
     end
-
-    private
-
-    def api_key
-      @api_key ||= ENV['OPENAI_API_KEY'] || ask('Please enter your OpenAI API key:  ') { |q| q.echo = "*" }
-    end
-  end
-
-  # Handling errors from OpenAI API requests and responses
-  module Error
-    def self.check_response(response)
-      if response.choices.empty?
-        raise PleaseGPT::Error, 'OpenAI API request returned empty response'
-      elsif response.choices[0].text.nil?
-        raise PleaseGPT::Error, 'OpenAI API request returned nil text'
-      end
-    end
-  end
-
-  # Module for OpenAI API requests
-  module Api
-    def self.generate_text(input)
-      response = OpenAI::Completion.create(
-        engine: 'davinci',
-        prompt: "#{input}\n",
-        max_tokens: 2048,
-        temperature: 0.5,
-        n: 1,
-        stop: "\n",
-        api_key: api_key
-      )
-      Error.check_response(response)
-      response.choices[0].text.strip.colorize(:blue)
-    end
-
-    private_class_method def self.api_key
-      @api_key ||= ENV['OPENAI_API_KEY'] || begin
-        ask('Please enter your OpenAI API key:  ') { |q| q.echo = '*' }
-      end
-    end
   end
 end
-# rubocop:enable Metrics/MethodLength
