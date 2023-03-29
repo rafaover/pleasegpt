b8e2971f04cc0ad73ba982e90601da3edccf893d
Rafael Moreira <rafamsilva@gmail.com>
2023-03-29 20:37:57 +1100

    removed unused code. - These methods are in lib/pleasegpt.rb

--- lib/pleasegpt/cli.rb
+++ lib/pleasegpt/cli.rb
@@ -1,32 +1,12 @@
 require 'pleasegpt'
 require 'colorize'
-require 'dotenv/load'
 
 module PleaseGPT
+  # CLI class for the command-line tool interface
   class CLI
-    def save_api_key
-      if Api.api_key
-        File.write('.openai', Api.api_key)
-        puts 'OpenAI API key saved to .openai file'
-      else
-        puts 'OpenAI API key not found in environment variables'
-      end
-    end
-
-    def load_api_key
-      if File.exist?('.openai')
-        key = File.read('.openai').strip
-        ENV['OPENAI_API_KEY'] = key
-        puts 'OpenAI API key loaded from .openai file'
-      else
-        puts 'No .openai file found'
-      end
-    end
-
     def start(args)
-      load_api_key
       if args.empty?
-        puts 'Please enter a valid command'
+        puts '>>> Please enter a valid command <<<'
       else
         input = args.join(' ')
         puts Api.generate_text(input).to_s.colorize(:blue)
