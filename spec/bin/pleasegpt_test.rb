4586ac60a5ad52fc84437372dc90bb5537cf08be
Rafael Moreira <rafamsilva@gmail.com>
2023-03-29 23:09:15 +1100

    update

--- spec/bin/pleasegpt_test.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe "pleasegpt executable" do
-  it "runs with the given arguments" do
-    expect { system('bin/pleasegpt how much is 2+2') }
-      .to output(/\S/).to_stdout
-      .and output(String).to_stdout
-  end
-end
