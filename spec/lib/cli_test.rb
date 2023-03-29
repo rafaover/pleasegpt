23293b3259cda6193c73c96006fa3116b8229504
Rafael Moreira <rafamsilva@gmail.com>
2023-03-29 13:29:04 +1100

    cli test

--- spec/lib/cli_test.rb
+++ spec/lib/cli_test.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 require 'pleasegpt/cli'
 
+require 'spec_helper'
+require 'pleasegpt/cli'
+
 RSpec.describe PleaseGPT::CLI do
   describe "#start" do
     context "when given a valid command" do
@@ -8,6 +11,7 @@ RSpec.describe PleaseGPT::CLI do
 
       it "returns a response from the OpenAI API" do
         expect { subject.start(args) }.to output(/.*\n/).to_stdout
+        expect { subject.start(args) }.to output(/\S/).to_stdout
       end
     end
   end
