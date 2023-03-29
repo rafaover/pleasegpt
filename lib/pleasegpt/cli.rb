89086fd0b69c087d5b5369c477a1a8029231c0e6
Rafael Moreira <rafamsilva@gmail.com>
2023-03-29 19:29:47 +1100

    cli.rb updated

--- lib/pleasegpt/cli.rb
+++ lib/pleasegpt/cli.rb
@@ -2,9 +2,28 @@ require 'pleasegpt'
 require 'colorize'
 
 module PleaseGPT
-  # Gem command-line tool interface
   class CLI
+    def save_api_key
+      if Api.api_key
+        File.write('.openai', Api.api_key)
+        puts 'OpenAI API key saved to .openai file'
+      else
+        puts 'OpenAI API key not found in environment variables'
+      end
+    end
+
+    def load_api_key
+      if File.exist?('.openai')
+        key = File.read('.openai').strip
+        ENV['OPENAI_API_KEY'] = key
+        puts 'OpenAI API key loaded from .openai file'
+      else
+        puts 'No .openai file found'
+      end
+    end
+
     def start(args)
+      load_api_key
       if args.empty?
         puts 'Please enter a valid command'
       else
