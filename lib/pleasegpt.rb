8c14e5264e3279e71717a0cfca76101765a38a77
Rafael Moreira <rafamsilva@gmail.com>
2023-03-29 17:06:08 +1100

    refactoedc code for pleasegpt, trying to save key

--- lib/pleasegpt.rb
+++ lib/pleasegpt.rb
@@ -1,6 +1,7 @@
 # rubocop:disable Metrics/MethodLength
 require 'openai'
 require 'highline/import'
+require 'yaml'
 require 'dotenv/load'
 
 # Module for PleaseGPT gem
@@ -9,14 +10,40 @@ module PleaseGPT
 
   # Module for OpenAI API requests
   module Api
+    CONFIG_PATH = File.join(File.dirname(__FILE__), '..', 'config', 'config.yml')
+    API_KEY_VAR_NAME = 'OPENAI_API_KEY'
+
     def self.api_key
-      @api_key = ENV['OPENAI_API_KEY'] || begin
-        ask('Please enter your OpenAI API key:  ') { |q| q.echo = '*' }
+      api_key = ENV[API_KEY_VAR_NAME]
+      api_key = copy_api_key_from_config_file if api_key.nil?
+
+      if api_key.nil?
+        api_key = ask_api_key_from_user
+        save_api_key_to_config_file(api_key)
       end
+
+      api_key
+    end
+
+    def self.copy_api_key_from_config_file
+      config = YAML.load_file(CONFIG_PATH)
+      config['OPENAI_API_KEY']
+    rescue StandardError
+      nil
+    end
+
+    def self.save_api_key_to_config_file(api_key)
+      config = YAML.load_file(CONFIG_PATH)
+      config['OPENAI_API_KEY'] = api_key
+      File.write(CONFIG_PATH, config.to_yaml)
+    end
+
+    def self.ask_api_key_from_user
+      ask('Please enter your OpenAI API key:  ') { |q| q.echo = '*' }
     end
 
     def self.generate_text(input)
-      client = OpenAI::Client.new(api_key: Api.api_key)
+      client = OpenAI::Client.new(api_key: api_key)
       response = client.completions(
         engine: 'text-davinci-003',
         prompt: "#{input}",
