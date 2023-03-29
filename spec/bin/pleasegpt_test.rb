0b9a956fed56656d2645593ff5ae1723a15d71cc
Rafael Moreira <rafamsilva@gmail.com>
2023-03-29 15:36:57 +1100

    tests updates

--- spec/bin/pleasegpt_test.rb
+++ spec/bin/pleasegpt_test.rb
@@ -1,38 +1,9 @@
-require 'pleasegpt/cli'
+require 'spec_helper'
 
-RSpec.describe PleaseGPT::CLI do
-  describe '#start' do
-    before do
-      allow(described_class).to receive(:print)
-      allow(described_class).to receive(:puts)
-    end
-
-    context 'when given valid input' do
-      let(:input) { "I'm feeling lucky" }
-      let(:output) { 'Some generated text' }
-
-      before do
-        allow(described_class).to receive(:gets).and_return(input)
-        allow(PleaseGPT::Api).to receive(:generate_text).with(input).and_return(output)
-      end
-
-      it 'calls generate_text with the input and prints the output' do
-        expect(described_class).to receive(:puts).with(output)
-        described_class.start([])
-      end
-    end
-
-    context 'when given empty input' do
-      let(:input) { '' }
-
-      before do
-        allow(described_class).to receive(:gets).and_return(input, 'I want some text')
-      end
-
-      it 'prompts user for valid input' do
-        expect(described_class).to receive(:puts).with('Please enter a valid command')
-        described_class.start([])
-      end
-    end
+RSpec.describe "pleasegpt executable" do
+  it "runs with the given arguments" do
+    expect { system('bin/pleasegpt how much is 2+2') }
+      .to output(/\S/).to_stdout
+      .and output(String).to_stdout
   end
 end
--- spec/lib/cli_test.rb
+++ spec/lib/cli_test.rb
@@ -5,7 +5,7 @@ require 'colorize'
 RSpec.describe PleaseGPT::CLI do
   describe "#start" do
     context "when given a valid command" do
-      let(:args) { ["how much is 2+2"] }
+      let(:args) { ["how much is 2+2?"] }
 
       it "returns a response from the OpenAI API, not empty, with blue colour" do
         expect { subject.start(args) }.to output(/.*\n/).to_stdout
