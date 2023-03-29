772a16e311e0464e86d2731c43d8a2768d3a75ac
Rafael Moreira <rafamsilva@gmail.com>
2023-03-29 12:36:17 +1100

    updated generate_Text test

--- spec/lib/pleasegpt_test.rb
+++ spec/lib/pleasegpt_test.rb
@@ -4,12 +4,11 @@ require 'pleasegpt'
 describe PleaseGPT::Api do
   describe '.generate_text' do
     context 'with valid input' do
-      let(:input) { 'What is the meaning of life?' }
+      let(:input) { 'which command do I use to fetch a github repository?' }
 
       it 'returns a non-empty response from the API' do
-        input = 'How much is 2+2?'
-        api_key = ENV['OPENAI_API_KEY']
-        output = PleaseGPT::Api.generate_text(input, api_key)
+        input = 'which command do I use to fetch a github repository?'.strip
+        output = PleaseGPT::Api.generate_text(input)
         expect(output).not_to be_empty
       end
     end
