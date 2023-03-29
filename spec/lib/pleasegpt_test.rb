af220cef679e8785d9c16448049432486137e715
Rafael Moreira <rafamsilva@gmail.com>
2023-03-30 00:18:59 +1100

    test to check if API_KEY is saved on .env

--- spec/lib/pleasegpt_test.rb
+++ spec/lib/pleasegpt_test.rb
@@ -13,4 +13,28 @@ describe PleaseGPT::Api do
       end
     end
   end
+
+  describe PleaseGPT::Api do
+    describe '.load_api_key' do
+      context 'with valid input' do
+        let(:key) { 'test-api-key' }
+
+        it 'saves the API key to the correct file' do
+          allow(PleaseGPT).to receive(:ask).and_return(key)
+          puts ">>>>> COPY/PASTE test-api-key as the key below"
+          puts ">>>>>"
+          PleaseGPT::Api.load_api_key
+          expect(File.read('lib/.openai')).to eq("OPENAI_API_KEY=#{key}")
+        end
+
+        it 'sets the OPENAI_API_KEY environment variable' do
+          allow(PleaseGPT).to receive(:ask).and_return(key)
+          puts ">>>>> COPY/PASTE test-api-key as the key below"
+          puts ">>>>>"
+          PleaseGPT::Api.load_api_key
+          expect(ENV['OPENAI_API_KEY']).to eq(key)
+        end
+      end
+    end
+  end
 end
