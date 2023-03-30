c05ecbe6c843441f0d887bd1279ea0b9721ff115
Rafael Moreira <rafamsilva@gmail.com>
2023-03-30 14:09:38 +1100

    updates bin test. Not working.

--- spec/bin/pleasegpt_test.rb
+++ spec/bin/pleasegpt_test.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe "pleasegpt executable" do
   it "runs with the given arguments" do
-    expect { system('bin/pleasegpt how much is 2+2') }
+    expect { system('bin/pleasegpt what is the capital of France?') }
       .to output(/\S/).to_stdout
       .and output(String).to_stdout
   end
