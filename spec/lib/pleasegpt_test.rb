43edfb8259146f5a5e9d9b0c55935290983cb01a
Rafael Moreira <rafamsilva@gmail.com>
2023-03-30 14:03:13 +1100

    updated tests

--- spec/lib/pleasegpt_test.rb
+++ spec/lib/pleasegpt_test.rb
@@ -1,38 +1,21 @@
 require 'spec_helper'
 require 'pleasegpt'
 
-describe PleaseGPT::Api do
-  describe '.generate_text' do
-    context 'with valid input' do
-      let(:input) { 'which command do I use to fetch a github repository?' }
+RSpec.describe PleaseGPT::Api do
+  describe '#generate_text' do
+    let(:input) { 'Some prompt for GPT' }
+    let(:response_text) { 'Generated text' }
 
-      it 'returns a non-empty response from the API' do
-        PleaseGPT::Api.save_api_key('test-api-key') # save the test API key
-        output = PleaseGPT::Api.generate_text(input)
-        expect(output).not_to be_empty
-      end
+    before do
+      allow_any_instance_of(OpenAI::Client)
+        .to receive(:completions)
+        .and_return('choices' => [{ 'text' => response_text }])
     end
-  end
 
-  describe '.save_api_key' do
-    it 'saves the API key to the correct file' do
-      key = 'test-api-key'
-      PleaseGPT::Api.save_api_key(key)
-      expect(File.read('lib/.openai')).to eq("OPENAI_API_KEY=#{key}")
-
-      # Cleanup the file after the test
-      File.delete('lib/.openai')
-    end
-  end
-
-  describe '.load_api_key' do
-    context 'with valid input' do
-      let(:key) { 'test-api-key' }
-
-      it 'sets the OPENAI_API_KEY environment variable' do
-        PleaseGPT::Api.load_api_key(key)
-        expect(ENV['OPENAI_API_KEY']).to eq(key)
-      end
+    it 'returns a non-empty response' do
+      response = PleaseGPT::Api.generate_text(input)
+      expect(response).not_to be_empty
+      expect(response).to eq(response_text)
     end
   end
 end
