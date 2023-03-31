786ec310fe5a4de932c2b9d98f6e7d182e269a63
Rafael Moreira <rafamsilva@gmail.com>
2023-03-31 13:01:02 +1100

    updated spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,15 +4,6 @@ require 'rspec'
 require 'pleasegpt'
 require 'dotenv/load'
 
-unless ENV['OPENAI_API_KEY']
-  # Prompt the user to enter their API key
-  puts 'Please enter your OpenAI API key: '
-  api_key = gets.chomp
-
-  # Set the environment variable for the current process
-  ENV['OPENAI_API_KEY'] = api_key
-end
-
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
