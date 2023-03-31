d33a0dd2cb2668977a509634d26563bcfc02faee
Rafael Moreira <rafamsilva@gmail.com>
2023-03-31 12:12:53 +1100

    added pleasegpt testing + update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'rspec'
 require 'pleasegpt'
 require 'dotenv/load'
@@ -12,6 +13,16 @@ unless ENV['OPENAI_API_KEY']
 end
 
 RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
   config.color = true
   config.formatter = :documentation
 end
