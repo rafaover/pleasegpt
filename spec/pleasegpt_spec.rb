eca6014d54a8058f58706cf489e203e0edd11fbd
Rafael Moreira <rafamsilva@gmail.com>
2023-03-31 14:00:00 +1100

    updated ci/cd files

--- spec/pleasegpt_spec.rb
+++ spec/pleasegpt_spec.rb
@@ -6,8 +6,4 @@ RSpec.describe Pleasegpt do
   it 'has a version number' do
     expect(Pleasegpt::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
