731693cb2fccefe72bf9209bb7c792858be245c4
Rafael Moreira <rafamsilva@gmail.com>
2023-07-27 11:29:58 +1000

    refactored testfile.

--- spec/lib/pleasegpt_test.rb
+++ spec/lib/pleasegpt_test.rb
@@ -3,8 +3,6 @@
 require 'spec_helper'
 require 'pleasegpt'
 
-# rubocop:disable Metrics/BlockLength
-
 RSpec.describe PleaseGPT::Api do
   describe 'Api' do
     context 'when API key is valid' do
@@ -34,35 +32,5 @@ RSpec.describe PleaseGPT::Api do
         expect { PleaseGPT::Api.load_api_key }.to output("API Key cannot be empty. Please try again.\n").to_stdout
       end
     end
-
-    describe '.generate_text' do
-      before do
-        allow(PleaseGPT::Api).to receive(:openai_client).and_return(client)
-        allow(PleaseGPT::Error).to receive(:check_response).and_return('output text')
-      end
-
-      let(:client) { double('client') }
-
-      it 'returns generated text' do
-        expect(client).to receive(:completions)
-        expect(PleaseGPT::Api.generate_text('prompt')).to eq('output text')
-      end
-    end
-
-    describe '.openai_client' do
-      let(:api_key) { 'key123' }
-      let(:client) { double('client') }
-
-      before do
-        ENV['OPENAI_API_KEY'] = api_key
-        allow(OpenAI::Client).to receive(:new).and_return(client)
-      end
-
-      it 'returns a new instance of OpenAI::Client' do
-        expect(PleaseGPT::Api.openai_client).to eq(client)
-      end
-    end
   end
 end
-
-# rubocop:enable Metrics/BlockLength
